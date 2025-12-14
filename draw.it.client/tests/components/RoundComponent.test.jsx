9f02ba00d07c9c35c2de5c1416035d97a08dbb91
Laimis Papinigis <laimis.papinigis@mif.stud.vu.lt>
2025-12-14 11:12:51 +0200

    Add test for RoundComponent

---
+++ draw.it.client/tests/components/RoundComponent.test.jsx
@@ -0,0 +1,15 @@
+﻿import React from "react";
+import { render, screen } from "@testing-library/react";
+import { describe, expect} from 'vitest';
+import '@testing-library/jest-dom';
+import RoundComponent from '@/components/gameplay/RoundComponent.jsx';
+
+describe("RoundComponent", () => {
+    test("renders currentRound and totalRounds when provided", () => {
+        render(<RoundComponent currentRound={3} totalRounds={5} />);
+
+        expect(screen.getByText("3 / 5")).toBeInTheDocument();
+
+        expect(screen.getByText("Round")).toBeInTheDocument();
+    });
+});
