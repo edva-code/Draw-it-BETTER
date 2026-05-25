8f32ee9e5cd7e2f9a91174d2e9055fb725627589
Augustinas Pletkus <cscoolfin@gmail.com>
2026-05-25 15:49:14 +0300

    Migrations fixed

--- Draw.it.Server/Migrations/20260525124453_AddEquippedTitle.Designer.cs
+++ Draw.it.Server/Migrations/20260525124453_AddEquippedTitle.Designer.cs
@@ -2,6 +2,7 @@
 using Draw.it.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Draw.it.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260525124453_AddEquippedTitle")]
+    partial class AddEquippedTitle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
