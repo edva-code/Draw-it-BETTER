4974fc2f21aa2c35759e5b18c60fc12a2e91199f
Augustinas Pletkus <cscoolfin@gmail.com>
2026-05-25 20:04:19 +0300

    createdAt Migration

---
+++ Draw.it.Server/Migrations/20260525170030_AddCreatedAtToUsers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Draw.it.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "NOW()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "users");
+        }
+    }
+}
