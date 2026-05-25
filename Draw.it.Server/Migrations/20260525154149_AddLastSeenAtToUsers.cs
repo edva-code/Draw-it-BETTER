8633f831797aa7b468aae79f9eb21c0f8c2ea14a
Augustinas Pletkus <cscoolfin@gmail.com>
2026-05-25 19:21:17 +0300

    Migration last seen

---
+++ Draw.it.Server/Migrations/20260525154149_AddLastSeenAtToUsers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Draw.it.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastSeenAtToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "last_seen_at",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "last_seen_at",
+                table: "users");
+        }
+    }
+}
